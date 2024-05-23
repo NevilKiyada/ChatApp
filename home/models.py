d9880d373b2bcb200ad17bd4bbec73da1a1ae73c
NevilKiyada <nevilkiyada04@gmail.com>
2024-05-23 14:04:07 +0530

    we

--- home/models.py
+++ home/models.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User  # Import the built-in User model
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User , on_delete=models.SET_NULL , null=True , blank= True) 
+    name=models.CharField(max_length=100)
+    pic=models.ImageField(upload_to=" userimg" , blank=True , null=True )
    
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+
+# class friemd(models.Model):
